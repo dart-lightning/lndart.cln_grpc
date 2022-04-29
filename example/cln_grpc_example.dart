8be0b2e2ffb53c68849a1595886f25ed230855ae
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-04-30 00:37:48 +0200

    init package
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ example/cln_grpc_example.dart
@@ -0,0 +1,6 @@
+import 'package:cln_grpc/cln_grpc.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/cln_grpc.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library cln_grpc;
+
+export 'src/cln_grpc_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
