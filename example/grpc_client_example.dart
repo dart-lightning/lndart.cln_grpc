dce117c9421ce7f3d4d04127fe66d54551723111
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-06-11 12:59:05 +0100

    repo: cleanup the refused and remove all the uneccessary code due the old one
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- example/grpc_client_example.dart
+++ example/grpc_client_example.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 import 'package:cln_grpc/cln_grpc.dart';
 
-Future<void> main(List<String>args) async{
-  var client = GRPCClient(rootPath: args[0],certClientPath: "/");
-  var response= await client.getinfo();
+Future<void> main(List<String> args) async {
+  var client = GRPCClient(rootPath: args[0], certClientPath: "/");
+  var response = await client.getinfo();
   print('Response from server\n$response');
   client.close();
 }
