6d65870738e4489353308f2fa217ad3a5d792485
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-06-17 00:11:40 +0100

    grpc: add simple test
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- example/grpc_client_example.dart
+++ example/grpc_client_example.dart
@@ -1,7 +1,7 @@
 import 'package:cln_grpc/cln_grpc.dart';
 
 Future<void> main(List<String> args) async {
-  var client = GRPCClient(rootPath: args[0], certClientPath: "/");
+  var client = GRPCClient(rootPath: args[0]);
   var response = await client.getinfo();
   print('Response from server\n$response');
 
