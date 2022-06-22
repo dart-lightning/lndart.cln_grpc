c55b015a27e62fefff5a43fdd3d024055832f921
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-06-22 10:02:37 +0100

    feat: added new services

--- example/grpc_client_example.dart
+++ example/grpc_client_example.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:cln_grpc/cln_grpc.dart';
 
 Future<void> main(List<String> args) async {
@@ -10,6 +12,28 @@ Future<void> main(List<String> args) async {
   response = await client.call<GetinfoRequest, GetinfoResponse>(
       method: "getinfo", payload: GetinfoRequest());
   print("Hello from node ${response.alias}");
+
+  var transactionList =
+      await client.call<ListtransactionsRequest, ListtransactionsResponse>(
+          method: "listTransactions", payload: ListtransactionsRequest());
+  print("Transactions of node");
+  print(transactionList.transactions);
+
+  var fundsList = await client.call<ListfundsRequest, ListfundsResponse>(
+      method: "listFunds", payload: ListfundsRequest());
+  print("List funds channels of node");
+  print(fundsList.channels);
+
+  var peersList = await client.call<ListpeersRequest, ListpeersResponse>(
+      method: "listPeers", payload: ListpeersRequest());
+  print("List Peers channels of node");
+  print(peersList.peers);
+
+  var channelList =
+      await client.call<ListchannelsRequest, ListchannelsResponse>(
+          method: "listChannels", payload: ListchannelsRequest());
+  print("List channels of node");
+  print(channelList.channels);
 
   client.close();
 }
