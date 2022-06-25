b50cf59f1b34d277479fdb574e9de9cc7be93460
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-06-25 13:45:41 +0100

    grpc: implement the common interface
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- example/grpc_client_example.dart
+++ example/grpc_client_example.dart
@@ -1,4 +1,20 @@
+import 'package:cln_common/cln_common.dart';
 import 'package:cln_grpc/cln_grpc.dart';
+
+class ListTransactionProxy extends Serializable {
+  ListtransactionsRequest proxy;
+
+  ListTransactionProxy(this.proxy);
+
+  factory ListTransactionProxy.build() =>
+      ListTransactionProxy(ListtransactionsRequest());
+
+  @override
+  Map<String, dynamic> toJSON() => proxy.writeToJsonMap();
+
+  @override
+  T as<T>() => proxy as T;
+}
 
 Future<void> main(List<String> args) async {
   var client = GRPCClient(rootPath: args[0]);
@@ -7,8 +23,8 @@ Future<void> main(List<String> args) async {
 
   /// It is possible also use the generic call like
   var transactionList =
-      await client.call<ListtransactionsRequest, ListtransactionsResponse>(
-          method: "listtransactions", payload: ListtransactionsRequest());
+      await client.call<ListTransactionProxy, ListtransactionsResponse>(
+          method: "listtransactions", params: ListTransactionProxy.build());
   print("Transactions of node");
   print(transactionList.transactions);
 
--- test/cln_client_test.dart
+++ test/cln_client_test.dart
@@ -1,7 +1,51 @@
 import 'dart:io';
 
+import 'package:cln_common/cln_common.dart';
 import 'package:cln_grpc/cln_grpc.dart';
 import 'package:test/test.dart';
+
+class GetInfoProxy extends Serializable {
+  GetinfoRequest proxy;
+
+  GetInfoProxy(this.proxy);
+
+  factory GetInfoProxy.build() => GetInfoProxy(GetinfoRequest());
+
+  @override
+  Map<String, dynamic> toJSON() => proxy.writeToJsonMap();
+
+  @override
+  T as<T>() => proxy as T;
+}
+
+class ListTransactionProxy extends Serializable {
+  ListtransactionsRequest proxy;
+
+  ListTransactionProxy(this.proxy);
+
+  factory ListTransactionProxy.build() =>
+      ListTransactionProxy(ListtransactionsRequest());
+
+  @override
+  Map<String, dynamic> toJSON() => proxy.writeToJsonMap();
+
+  @override
+  T as<T>() => proxy as T;
+}
+
+class ListChannelProxy extends Serializable {
+  ListchannelsRequest proxy;
+
+  ListChannelProxy(this.proxy);
+
+  factory ListChannelProxy.build() => ListChannelProxy(ListchannelsRequest());
+
+  @override
+  Map<String, dynamic> toJSON() => proxy.writeToJsonMap();
+
+  @override
+  T as<T>() => proxy as T;
+}
 
 void main() {
   var env = Platform.environment;
@@ -9,8 +53,8 @@ void main() {
   group('Generic client tests', () {
     test('call get info through generic client', () async {
       var client = GRPCClient(rootPath: tlsPath);
-      var response = await client.call<GetinfoRequest, dynamic>(
-          method: "getinfo", payload: GetinfoRequest());
+      var response = await client.call<GetInfoProxy, dynamic>(
+          method: "getinfo", params: GetInfoProxy.build());
       expect(response, isNotNull);
       expect(response.alias, "clighting4j-node");
     });
@@ -18,17 +62,16 @@ void main() {
     test('call list transaction through generic client', () async {
       var client = GRPCClient(rootPath: tlsPath);
       var response =
-          await client.call<ListtransactionsRequest, ListtransactionsResponse>(
-              method: "listtransactions", payload: ListtransactionsRequest());
+          await client.call<ListTransactionProxy, ListtransactionsResponse>(
+              method: "listtransactions", params: ListTransactionProxy.build());
       expect(response, isNotNull);
       expect(response.transactions.isEmpty, isTrue);
     });
 
     test('call list channels through generic client', () async {
       var client = GRPCClient(rootPath: tlsPath);
-      var response =
-          await client.call<ListchannelsRequest, ListchannelsResponse>(
-              method: "listchannels", payload: ListchannelsRequest());
+      var response = await client.call<ListChannelProxy, ListchannelsResponse>(
+          method: "listchannels", params: ListChannelProxy.build());
       expect(response, isNotNull);
       expect(response.channels.isEmpty, isTrue);
     });
