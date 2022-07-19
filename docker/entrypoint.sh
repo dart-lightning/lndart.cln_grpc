f28f7a5bf1939ded5a7e589230a89a739fbd5acc
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-07-19 11:12:11 +0000

    test: required an invoice with unix socket
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,4 +3,4 @@ ls -la
 # Give time to cln to boostrap
 sleep 5m
 make dep
-make TLS_PATH=/.lightning/testnet/ check
+make TLS_PATH=/.lightning/testnet/ RPC_PATH=/.lightning/testnet/lightning-rpc check
